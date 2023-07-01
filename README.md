d516092c09aef504664a6983f9146f5fd9465d9b
Olatunji Ruth <rutherfordola@gmail.com>
2023-07-01 10:09:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Blu
+ Blu is  mobile wallet app for sending and receiving of money, 
---
+++ android/app/src/main/kotlin/com/techorb/blu/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.techorb.blu
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
